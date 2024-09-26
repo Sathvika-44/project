a244c3dad2e7ed8f2d17fec223bd27973c9edb42
Sathvika-44 <61205509+Sathvika-44@users.noreply.github.com>
2024-09-26 10:45:46 +0530

    Add files via upload

---
+++ QuizApp/QuizApp/src/main/java/com/example/QuizApp/QuestionController.java
@@ -0,0 +1,35 @@
+package com.example.QuizApp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+	 @Autowired
+	 QuestionService questionService;
+	 
+	@GetMapping("allQuestions")
+	public ResponseEntity<List<Question>> getAllQuestions() {
+		return  questionService.getAllQuestions();
+	}
+	
+	@GetMapping("category/{category}")
+	public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable("category") String category) {
+		return  questionService.getQuestionsByCategory(category);
+	}
+	@PostMapping(path="add", consumes= "application/json")
+	public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+		return questionService.addQuestion(question);
+	}
+	
+}
